2d6f5cfc0d68c95a420a4935f5804bc64f73bcb9
BlackNeo <webscribo@gmail.com>
2021-05-06 11:40:55 +0200

    Heroku configuration

--- ng-admin/server.js
+++ ng-admin/server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const cors = require('cors');
+
+require('dotenv').config();
 
 const users = require("./routes/api/users");
 
 const app = express();
+
+app.use(cors());
 
 // BodyParser middleware
 app.use(bodyParser.urlencoded ({
@@ -36,5 +41,15 @@ require("./config/passport") (passport);
 app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
