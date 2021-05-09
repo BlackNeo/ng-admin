5def01d582ba90e918ae1d52fe3f1efecc4c1e65
BlackNeo <webscribo@gmail.com>
2021-05-09 18:12:04 +0200

    Heroku deploy

--- server.js
+++ server.js
@@ -34,5 +34,15 @@ require("./config/passport") (passport);
 app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // for Heroku deploy
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 app.listen(port, () => console.log(`Server up and running on port : ${port}.`));
