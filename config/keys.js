cc5275303c818808e8829a04987a496a3c7b6356
BlackNeo <webscribo@gmail.com>
2021-05-09 14:42:19 +0200

    ng-blog heroku deploy

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://webo:1234@cluster0.r9dta.mongodb.net/ng-admin?retryWrites=true&w=majority'
+};
--- ng-admin/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://webo:1234@cluster0.r9dta.mongodb.net/ng-admin?retryWrites=true&w=majority;',
-    secretOrKey: "secret"
-  };
