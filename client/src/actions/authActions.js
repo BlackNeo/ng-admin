4ee7421f8392c4e3cce81cd876c8b961f7c7df8e
BlackNeo <webscribo@gmail.com>
2021-05-12 21:49:06 +0200

    Login bug fix

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import setAuthToken from "../utils/setAuthToken";
 import jwt_decode from "jwt-decode";
-
 import {
     GET_ERRORS,
     SET_CURRENT_USER,
@@ -12,16 +11,16 @@ import {
 export const registerUser = (userData, history) => dispatch => {
     axios
         .post("/api/users/register", userData)
-        .then(res => history.push("/login")) // redirect to login on success register
-        .catch(err => 
-            dispatch({ 
+        .then(res => history.push("/login")) // re-direct to login on successful register
+        .catch(err =>
+            dispatch({
                 type: GET_ERRORS,
                 payload: err.response.data
             })
-        );
+    );
 };
 
-// Login - get User Token
+// Login - get user token
 export const loginUser = userData => dispatch => {
     axios
         .post("/api/users/login", userData)
@@ -30,22 +29,20 @@ export const loginUser = userData => dispatch => {
             // Set token to localStorage
             const { token } = res.data;
             localStorage.setItem("jwtToken", token);
-            // Set token to Auht header
+            // Set token to Auth header
             setAuthToken(token);
             // Decode token to get user data
             const decoded = jwt_decode(token);
             // Set current user
             dispatch(setCurrentUser(decoded));
         })
-        .catch(err =>
-            dispatch({
-                type: GET_ERRORS,
-                payload: err.response.data
-            })
-        );
-}
-
-
+    .catch(err =>
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    );
+};
 // Set logged in user
 export const setCurrentUser = decoded => {
     return {
@@ -53,20 +50,18 @@ export const setCurrentUser = decoded => {
         payload: decoded
     };
 };
-
-// User laoding
+// User loading
 export const setUserLoading = () => {
     return {
         type: USER_LOADING
     };
 };
-
 // Log user out
-export const logoutUser = () => dispacth => {
+export const logoutUser = () => dispatch => {
     // Remove token from local storage
     localStorage.removeItem("jwtToken");
     // Remove auth header for future requests
     setAuthToken(false);
-    // Set current user to empty 
-    dispacth(setCurrentUser({}));
+    // Set current user to empty object {} which will set isAuthenticated to false
+    dispatch(setCurrentUser({}));
 };
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -6,7 +6,7 @@ import { loginUser } from "../../actions/authActions";
 import classnames from "classnames";
 
 class Login extends Component {
-    constructor () {
+    constructor() {
         super();
         this.state = {
             email: "",
@@ -16,25 +16,18 @@ class Login extends Component {
     }
 
     componentWillReceiveProps(nextProps) {
-        if (nextProps.auth.isAuthenticed) {
-            this.props.history.push("/dashboard"); // push user to Dashboard when login
+        if (nextProps.auth.isAuthenticated) {
+            this.props.history.push("/dashboard"); // push user to dashboard when they login
         }
         if (nextProps.errors) {
-            this.state({
+            this.setState({
                 errors: nextProps.errors
             });
         }
     }
 
-    componentDidMount() {
-        // If logged in and user navigates to Login redirect to Dashboard
-        if (this.props.auth.isAuthenticed) {
-            this.props.history.push("/dashboard");
-        }
-    }
-
     onChange = e => {
-        this.setState({ [e.target.id]: e.target.value});
+        this.setState({ [e.target.id]: e.target.value });
     };
 
     onSubmit = e => {
@@ -43,26 +36,24 @@ class Login extends Component {
             email: this.state.email,
             password: this.state.password
         };
-
-        this.props.loginUser(userData); // Don't need to pass this.props.history as paramaters
+        this.props.loginUser(userData); // since we handle the redirect within our component, we don't need to pass in this.props.history as a parameter
     };
 
     render() {
         const { errors } = this.state;
-
         return (
             <div className="container">
-                <div style={{ marginTop: "4rem"}} className="row">
+                <div style={{ marginTop: "4rem" }} className="row">
                     <div className="col s8 offset-s2">
-                        <Link to="/" className="btn-flat wawes-effect">
-                            <i className="material-icons left">keybord_backspace</i>
-                            Back to home
+                        <Link to="/" className="btn-flat waves-effect">
+                            <i className="material-icons left">keyboard_backspace</i> Back to home
                         </Link>
-                        <div className="col s12" style={{ paddingLeft: "11.250px"}}>
-                            <h4><b>Login</b> below</h4>
+                        <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                            <h4>
+                                <b>Login</b> below
+                            </h4>
                             <p className="grey-text text-darken-1">
-                                Don't have an account ? 
-                                <Link to="/register">Register</Link>
+                                Don't have an account? <Link to="/register">Register</Link>
                             </p>
                         </div>
                         <form noValidate onSubmit={this.onSubmit}>
@@ -84,7 +75,7 @@ class Login extends Component {
                                 </span>
                             </div>
                             <div className="input-field col s12">
-                                <input 
+                                <input
                                     onChange={this.onChange}
                                     value={this.state.password}
                                     error={errors.password}
@@ -100,8 +91,8 @@ class Login extends Component {
                                     {errors.passwordincorrect}
                                 </span>
                             </div>
-                            <div className="col s12" style={{ paddingLeft: "11.250px"}}>
-                                <button 
+                            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                                <button
                                     style={{
                                         width: "150px",
                                         borderRadius: "3px",
@@ -111,7 +102,7 @@ class Login extends Component {
                                     type="submit"
                                     className="btn btn-large waves-effect waves-light hoverable blue accent-3"
                                 >
-                                    Login
+                                Login
                                 </button>
                             </div>
                         </form>
@@ -126,7 +117,7 @@ Login.propTypes = {
     loginUser: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     errors: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
     auth: state.auth,
@@ -135,5 +126,5 @@ const mapStateToProps = state => ({
 
 export default connect(
     mapStateToProps,
-    { loginUser} 
-) (Login);
+    { loginUser }
+)(Login);
